7d45789ffd513047069dc895dd97a7308ce5aa9f
StHung <phamthehung2212000@gmail.com>
2024-12-23 11:12:21 +0700

    remove unused code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,7 @@
-import { useState } from "react";
-import reactLogo from "../assets/react.svg";
-import viteLogo from "../../public/vite.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Button variant="contained">Hello world</Button>
